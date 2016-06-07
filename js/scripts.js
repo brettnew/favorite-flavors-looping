c9a9c98791d3cebfe30599804ec41e61b1fc37a7
Brian Grant and Brett New <brett@>
2016-06-07 09:37:01 -0700

    It will now use a for loop instead of a for each loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   $("#submit").click(function(event){
     var flavors = ["Chocolate", "Cookies 'n Cream", "Vanilla"];
     alert(flavors);
-    debugger;
-    flavors.forEach(function(flavor){
-      $(".favorite-flavors").append("<li>" + flavor + "</li>");
+    // debugger;
+    for (var index = 0; index < flavors.length; index++){
+      $(".favorite-flavors").append("<li>" + flavors[index] + "</li>");
 
       event.preventDefault();
 
-    });
+    };
   });
 });
